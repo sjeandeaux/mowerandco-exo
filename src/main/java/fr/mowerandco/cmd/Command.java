ee29e10e1482d9d38790d1c4a126213135615cd3
Stéphane Jeandeaux <stephane.jeandeaux@gmail.com>
2015-02-22 14:14:30 +0100

    [mowerandco]    ajout des commandes de la tondeuse

---
+++ src/main/java/fr/mowerandco/cmd/Command.java
@@ -0,0 +1,82 @@
+package fr.mowerandco.cmd;
+
+import fr.mowerandco.core.Lawn;
+import fr.mowerandco.core.Mower;
+
+import java.util.Optional;
+
+/**
+ * Ensemble des commandes possibles d'une tondeuse.
+ */
+public enum Command {
+
+    /**
+     * Rotation a gauche
+     */
+    LEFT('G') {
+        @Override
+        public void execute(final Lawn lawn, final Mower mower) {
+            mower.turnLeft();
+        }
+    },
+    /**
+     * Rotation a droite
+     */
+    RIGHT('D') {
+        @Override
+        public void execute(final Lawn lawn, final Mower mower) {
+            mower.turnRight();
+        }
+    },
+    /**
+     * On avance la tondeuse.
+     */
+    MOVE('A') {
+        @Override
+        public void execute(final Lawn lawn, final Mower mower) {
+            lawn.move(mower);
+        }
+    };
+
+    /**
+     * La lettre representant la commande.
+     */
+    private final Character shortName;
+
+    /**
+     * Initialise la lettre de la commande.
+     *
+     * @param shortName le nom court.
+     */
+    private Command(final char shortName) {
+        this.shortName = shortName;
+    }
+
+    /**
+     * Recherche la commande associe a la lettre
+     *
+     * @param shortName la lettre a rechercher
+     * @return la commande ou empty.
+     */
+    public static Optional<Command> valueOfByShortName(final Character shortName) {
+        final Command[] commands = Command.values();
+        Command command = null;
+        for (int i = 0; i < commands.length; i++) {
+            if (commands[i].shortName.equals(shortName)) {
+                command = commands[i];
+                break;
+            }
+        }
+        return Optional.ofNullable(command);
+    }
+
+    /**
+     * Execute une commande sur la tondeuse.
+     *
+     * @param mower la tondeuse sur laquelle on execute la commande.
+     * @param lawn  le terrain.
+     */
+    public abstract void execute(final Lawn lawn, final Mower mower);
+
+
+}
---
+++ src/test/java/fr/mowerandco/cmd/CommandTest.java
@@ -0,0 +1,68 @@
+package fr.mowerandco.cmd;
+
+import fr.mowerandco.core.Lawn;
+import fr.mowerandco.core.Mower;
+import org.easymock.EasyMock;
+import org.easymock.IMocksControl;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Optional;
+
+/**
+ * fr.mowerandco.core.cmd.Command
+ */
+public class CommandTest {
+
+    @DataProvider(name = "dataProviderMyValueOf")
+    public Object[][] dataProviderMyValueOf() throws Exception {
+        return new Object[][]{
+                {'G', Optional.of(Command.LEFT)},//
+                {'D', Optional.of(Command.RIGHT)},//
+                {'A', Optional.of(Command.MOVE)},//
+
+                {null, Optional.empty()},//
+                {'V', Optional.empty()},//
+                //
+        };
+    }
+
+    @Test(dataProvider = "dataProviderMyValueOf")
+    public void should_be_equals_valueOfByShortName(Character search, Optional<Command> expected) {
+        Assert.assertEquals(Command.valueOfByShortName(search), expected);
+    }
+
+
+    @Test
+    public void should_rotate_left_my_mower() {
+        IMocksControl controller = EasyMock.createControl();
+        Mower mower = controller.createMock(Mower.class);
+        mower.turnLeft();
+        controller.replay();
+        Command.LEFT.execute(null, mower);
+        controller.verify();
+    }
+
+
+    @Test
+    public void should_rotate_right_my_mower() {
+        IMocksControl controller = EasyMock.createControl();
+        Mower mower = controller.createMock(Mower.class);
+        mower.turnRight();
+        controller.replay();
+        Command.RIGHT.execute(null, mower);
+        controller.verify();
+    }
+
+    @Test
+    public void should_move_my_mower() {
+        IMocksControl controller = EasyMock.createControl();
+        Mower mower = controller.createMock(Mower.class);
+        Lawn lawn = controller.createMock(Lawn.class);
+        lawn.move(mower);
+        controller.replay();
+        Command.MOVE.execute(lawn, mower);
+        controller.verify();
+    }
+}
